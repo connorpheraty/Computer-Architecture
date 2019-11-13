bf4fcabf51d4f4c28696ef7f3f03568851c72ba2
Connor Heraty <connorpheraty@gmail.com>
2019-11-13 17:30:07 -0600

    Day 3 Assignment Complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,19 +2,6 @@ import sys
 
 """CPU functionality."""
 
-class Stack():
-    def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-    def size(self):
-        return len(self.stack)
-
 class CPU:
     """Main CPU class."""
 
@@ -75,6 +62,7 @@ class CPU:
         
         else:
             raise Exception("Unsupported ALU operation")
+
 
     def trace(self):
         """
